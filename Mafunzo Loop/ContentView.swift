c4f944d3cb442b0ef5db2772223025c612a9e928
joshua kilaha <jokilaha191@gmail.com>
2022-06-30 21:49:40 +0300

    Initial Commit

---
+++ Mafunzo Loop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mafunzo Loop
+//
+//  Created by mroot on 30/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mafunzo Loop/Mafunzo_LoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mafunzo_LoopApp.swift
+//  Mafunzo Loop
+//
+//  Created by mroot on 30/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Mafunzo_LoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
