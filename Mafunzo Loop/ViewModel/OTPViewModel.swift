eeb102e6c97029900df515adb939a0557f9013f9
joshua kilaha <jokilaha191@gmail.com>
2022-07-08 20:52:20 +0300

    Fetch Schools from Db Setup Account account type updated

--- Mafunzo Loop/ViewModel/OTPViewModel.swift
+++ Mafunzo Loop/ViewModel/OTPViewModel.swift
@@ -10,11 +10,17 @@ import SwiftUI
 import Firebase
 
 class OTPViewModel: ObservableObject {
-    @Published var user = User()
+    @Published var user: User = .init()
     //OTP
     @Published var otpText: String = ""
     @Published var otpFields: [String] = Array(repeating: "", count: 6)
     @Published var countryCode = ""
+    
+    @Published var accountType = ["PARENT", "TEACHER", "STUDENT", "BUS_DRIVER"]
+    @Published var accountSelected = 0
+    @Published var schoolSelected = 0
+    // VIEWS
+    @Published var toAccountSetup = false
     // MARK: Error
     @Published var showAlert: Bool = false
     @Published var errorMsg = ""
@@ -23,6 +29,13 @@ class OTPViewModel: ObservableObject {
     @Published var isLoading: Bool = false
     @Published var verificationTag: String?
     @AppStorage("log_status") var log_status = false
+    //Firebase
+    let db = Firestore.firestore()
+    //Initialze functions
+    init() {
+        getSchools()
+        //getAccountType()
+    }
     // MARK: Send OTP
     func sendOTP(phone: String) async {
         do {
@@ -49,10 +62,68 @@ class OTPViewModel: ObservableObject {
             _ = try await Auth.auth().signIn(with: credential)
             DispatchQueue.main.async {[self] in
                 isLoading = false
-                log_status = true
+                toAccountSetup = true
             }
         } catch {
             handleError(error: error.localizedDescription)
+        }
+    }
+    // MARK: Get Schools
+    func getSchools() {
+        //remove all schools
+        user.schools.removeAll()
+        let ref = db.collection("app_settings").document("schools").collection("KE")
+        ref.getDocuments { snapshot, error in
+            guard error == nil else {
+                print("Error!!! \(error!.localizedDescription)")
+                self.handleError(error: error!.localizedDescription)
+                return
+            }
+            if let snapshot = snapshot {
+                for document in snapshot.documents {
+                    let data = document.data()
+                    let schoolDetails = data["schoolName"] as? String ?? ""
+                    self.user.schools.append(schoolDetails)
+                }
+            }
+        }
+    }
+    // MARK: Get Account Type (Not able to fetch!!!!!)
+    func getAccountType() {
+        db.collection("app_settings").whereField("account_types", isEqualTo: true)
+            .getDocuments() { (querySnapshot, err) in
+                if let err = err {
+                    print("Error getting documents: \(err)")
+                } else {
+                    for document in querySnapshot!.documents {
+                        print("Account Data is: \(document.documentID) => \(document.data())")
+                    }
+                }
+        }
+    }
+    // MARK: SETUP ACCOUNT
+    func setupAccount(number: String, user: User, school: [String]) {
+            print("Phone number At setup Account \(number)")
+            isLoading = true
+            let db = Firestore.firestore()
+            let ref = db.collection("users").document(number)
+            ref.setData([
+                "accountType": user.accountType,
+                "dateCreated": user.dateCreated,
+                "email": user.email,
+                "firstName": user.firstName,
+                "lastName": user.lastName,
+                "profilePic": user.profilePic,
+                "schools": school
+            ]) { error in
+                if let error = error {
+                    self.handleError(error: error.localizedDescription)
+                    print(error.localizedDescription)
+                }
+            }
+        DispatchQueue.main.async {[self] in
+            isLoading = false
+            log_status = true
         }
     }
     // MARK: ERROR HANDLER
