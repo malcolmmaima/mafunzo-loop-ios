9f446f90a4b2aa93e8b4941daeef5619614542e9
joshua kilaha <jokilaha191@gmail.com>
2022-07-22 14:17:40 +0300

    Category cell replaced with Home Cell

--- Mafunzo Loop/View/HomeView.swift
+++ Mafunzo Loop/View/HomeView.swift
@@ -10,41 +10,45 @@ import SwiftUI
 struct HomeView: View {
     @EnvironmentObject var userViewModel: UserViewModel
     var body: some View {
-        GeometryReader { geo in
-            VStack {
-                HomeTopView()
-                    .frame(height: geo.size.height * 0.25)
+        NavigationView {
+            GeometryReader { geo in
                 VStack {
-                    ScrollView {
-                        CategoryCell(image: "ic_announcement", CategoryTitle: "Announcements", image2: "ic_calendar", CategoryTitle2: "Calendar")
-                        CategoryCell(image: "ic_requests", CategoryTitle: "Requests", image2: "ic_teachers", CategoryTitle2: "Teachers")
-                        CategoryCell(image: "ic_school_bus", CategoryTitle: "School Bus", image2: "ic_contacts", CategoryTitle2: "Contacts")
+                    HomeTopView()
+                        .frame(height: geo.size.height * 0.25)
+                    VStack {
+                        ScrollView {
+                            HomeCell()
+    //                        CategoryCell(image: "ic_announcement", CategoryTitle: "Announcements", image2: "ic_calendar", CategoryTitle2: "Calendar")
+    //                        CategoryCell(image: "ic_requests", CategoryTitle: "Requests", image2: "ic_teachers", CategoryTitle2: "Teachers")
+    //                        CategoryCell(image: "ic_school_bus", CategoryTitle: "School Bus", image2: "ic_contacts", CategoryTitle2: "Contacts")
+                        }
+                        .frame(height: geo.size.height * 0.7)
+                       .offset(y: -50)
+                        Button {
+                            print("")
+                        } label: {
+                            Text(userViewModel.schoolD.schoolName)
+                                .font(.body)
+                                .foregroundColor(.black)
+                                .frame(width: 350, height: 35)
+                        }
+                        .background(
+                            RoundedCornersShape(corners: .allCorners, radius: 20)
+                                .fill(Color.homeCategory)
+                        )
+                        .shadow(radius: 1)
                     }
-                    .frame(height: geo.size.height * 0.7)
-                   .offset(y: -50)
-                    Button {
-                        print("")
-                    } label: {
-                        Text(userViewModel.schoolD.schoolName)
-                            .font(.body)
-                            .foregroundColor(.black)
-                            .frame(width: 350, height: 35)
-                    }
-                    .background(
-                        RoundedCornersShape(corners: .allCorners, radius: 20)
-                            .fill(Color.homeCategory)
-                    )
-                    .shadow(radius: 1)
+                    .padding()
+                 // MARK: Curving view
+                     .background(
+                         RoundedCornersShape(corners: .allCorners, radius: 45)
+                            .fill(Color.ViewBackground)
+                     )
+                     .offset(y: -50)
                 }
-                .padding()
-             // MARK: Curving view
-                 .background(
-                     RoundedCornersShape(corners: .allCorners, radius: 45)
-                        .fill(Color.ViewBackground)
-                 )
-                 .offset(y: -50)
+                .background(Color.ViewBackground)
+                .navigationBarHidden(true)
             }
-            .background(Color.ViewBackground)
             .navigationBarHidden(true)
         }
     }
@@ -77,11 +81,17 @@ struct CategoryCell: View {
     var body: some View {
         HStack {
             VStack {
-                Image(image)
-                    .resizable()
-                    .frame(width: 80, height: 80)
-                Text(CategoryTitle)
-                    .font(.body)
+                Button {
+                    print("Clicked")
+                } label: {
+                    VStack {
+                        Image(image)
+                            .resizable()
+                            .frame(width: 80, height: 80)
+                        Text(CategoryTitle)
+                            .font(.body)
+                    }
+                }
             } .padding()
                 .frame(width: 160, height: 160)
             .background(
@@ -110,8 +120,156 @@ struct CategoryCell: View {
     }
 }
 
+struct HomeCell: View {
+    @State var toAccouncement = false
+    var body: some View {
+       // NavigationView {
+            VStack {
+                HStack {
+                    // MARK: Announcements
+                    NavigationLink {
+                        AnnoucementView()
+                    } label: {
+                        VStack {
+                            Image("ic_announcement")
+                                .resizable()
+                                .frame(width: 80, height: 80)
+                            Text("Announcements")
+                                .font(.body)
+                        }
+                    }
+                    .foregroundColor(Color.buttonHomeColor)
+                    .padding()
+                    .frame(width: 160, height: 160)
+                    .background(
+                    RoundedCornersShape(corners: .allCorners, radius: 20)
+                        .fill(Color.homeCategory)
+                    )
+                    .shadow(radius: 1)
+                    Spacer()
+                    // MARK: Calendar
+                    Button {
+                        print("")
+                    } label: {
+                        VStack {
+                            Image("ic_calendar")
+                                .resizable()
+                                .frame(width: 80, height: 80)
+                            Text("Calendar")
+                                .font(.body)
+                        }
+                    }
+                    .foregroundColor(Color.buttonHomeColor)
+                    .padding()
+                    .frame(width: 160, height: 160)
+                    .background(
+                    RoundedCornersShape(corners: .allCorners, radius: 20)
+                        .fill(Color.homeCategory)
+                    )
+                    .shadow(radius: 1)
+                }
+                    .padding(.horizontal, 10)
+                    .padding(.bottom, 8)
+                    .background(Color.clear)
+                HStack {
+                    // MARK: Requests
+                    Button {
+                        print("")
+                    } label: {
+                        VStack {
+                            Image("ic_requests")
+                                .resizable()
+                                .frame(width: 80, height: 80)
+                            Text("Requests")
+                                .font(.body)
+                        }
+                    }
+                    .foregroundColor(Color.buttonHomeColor)
+                    .padding()
+                    .frame(width: 160, height: 160)
+                    .background(
+                    RoundedCornersShape(corners: .allCorners, radius: 20)
+                        .fill(Color.homeCategory)
+                    )
+                    .shadow(radius: 1)
+                    Spacer()
+                    // MARK: Teachers
+                    Button {
+                        print("")
+                    } label: {
+                        VStack {
+                            Image("ic_teachers")
+                                .resizable()
+                                .frame(width: 80, height: 80)
+                            Text("Teachers")
+                                .font(.body)
+                        }
+                    }
+                    .foregroundColor(Color.buttonHomeColor)
+                    .padding()
+                    .frame(width: 160, height: 160)
+                    .background(
+                    RoundedCornersShape(corners: .allCorners, radius: 20)
+                        .fill(Color.homeCategory)
+                    )
+                    .shadow(radius: 1)
+                }
+                    .padding(.horizontal, 10)
+                    .padding(.bottom, 8)
+                    .background(Color.clear)
+                HStack {
+                    // MARK: Bus
+                    Button {
+                        print("")
+                    } label: {
+                        VStack {
+                            Image("ic_school_bus")
+                                .resizable()
+                                .frame(width: 80, height: 80)
+                            Text("Bus")
+                                .font(.body)
+                        }
+                    }
+                    .foregroundColor(Color.buttonHomeColor)
+                    .padding()
+                    .frame(width: 160, height: 160)
+                    .background(
+                    RoundedCornersShape(corners: .allCorners, radius: 20)
+                        .fill(Color.homeCategory)
+                    )
+                    .shadow(radius: 1)
+                    Spacer()
+                    // MARK: Contacts
+                    Button {
+                        print("")
+                    } label: {
+                        VStack {
+                            Image("ic_contacts")
+                                .resizable()
+                                .frame(width: 80, height: 80)
+                            Text("Contacts")
+                                .font(.body)
+                        }
+                    }
+                    .foregroundColor(Color.buttonHomeColor)
+                    .padding()
+                    .frame(width: 160, height: 160)
+                    .background(
+                    RoundedCornersShape(corners: .allCorners, radius: 20)
+                        .fill(Color.homeCategory)
+                    )
+                    .shadow(radius: 1)
+                }
+                    .padding(.horizontal, 10)
+                    .padding(.bottom, 8)
+                    .background(Color.clear)
+            }
+    }
+}
+
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-            HomeView()
+            //HomeView()
+        HomeCell()
     }
 }
