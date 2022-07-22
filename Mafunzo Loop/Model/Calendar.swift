ef56cd072e319d73b1dc93bf30d3f6d696a11d29
joshua kilaha <jokilaha191@gmail.com>
2022-07-22 21:26:52 +0300

    Calendar Model

---
+++ Mafunzo Loop/Model/Calendar.swift
@@ -0,0 +1,17 @@
+//
+//  Calendar.swift
+//  Mafunzo Loop
+//
+//  Created by Mroot on 22/07/2022.
+//
+
+import Foundation
+
+struct Calendar: Codable {
+    let id: String
+    let description: String
+    let end: String
+    let start: String
+    let location: String
+    let title: String
+}
