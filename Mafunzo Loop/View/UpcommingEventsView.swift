12015fa7d3910bbbc293987f9307a4f82ac78537
joshua kilaha <jokilaha191@gmail.com>
2022-07-26 15:16:10 +0300

    Upcoming Events View Init

---
+++ Mafunzo Loop/View/UpcommingEventsView.swift
@@ -0,0 +1,51 @@
+//
+//  UpcommingEventsView.swift
+//  Mafunzo Loop
+//
+//  Created by Mroot on 26/07/2022.
+//
+
+import SwiftUI
+
+struct UpcommingEventsView: View {
+    @StateObject var calendarViewModel = CalendarViewModel()
+    var body: some View {
+        GeometryReader { geo in
+            VStack {
+                AnnoucementTopView()
+                    .frame(height: geo.size.height * 0.09)
+                VStack {
+                    List {
+                         ForEach(calendarViewModel.calendarEvents, id: \.id) { event in
+                                      EventListViewCell(date: event.start, title: event.title, startingTime: event.start, ending: event.end)
+                                 .listRowSeparator(.hidden)
+                         }
+                         .listRowBackground(Color.ViewBackground)
+                     }
+                    .listStyle(.plain)
+                    .padding(.top, 25)
+                    .refreshable {
+                        calendarViewModel.showUpcommingEvents()
+                    }
+                }
+                //.padding()
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .background(
+                    RoundedCornersShape(corners: .allCorners, radius: 40)
+                        .fill(Color.ViewBackground)
+                )
+                .offset(y: -55)
+            }
+        }
+        .onAppear {
+            calendarViewModel.showUpcommingEvents()
+        }
+        .navigationBarTitle(Text("Upcomming Events"), displayMode: .inline)
+    }
+}
+
+struct UpcommingEventsView_Previews: PreviewProvider {
+    static var previews: some View {
+        UpcommingEventsView()
+    }
+}
