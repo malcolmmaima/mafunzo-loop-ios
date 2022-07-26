ca1250932504679f5d292a83fcf58f8ee5b2ce6d
joshua kilaha <jokilaha191@gmail.com>
2022-07-26 13:17:17 +0300

    Convert Date to Millisecond

--- Mafunzo Loop/Extensions.swift
+++ Mafunzo Loop/Extensions.swift
@@ -56,6 +56,15 @@ extension View {
         modifier(TextFieldStyle())
     }
 }
+// MARK: DATE
+extension Date {
+    var millisecondsSince1970: Int64 {
+        Int64((self.timeIntervalSince1970 * 1000.0))
+    }
+    init(milliseconds: Int64) {
+        self = Date(timeIntervalSince1970: TimeInterval(milliseconds) / 1000)
+    }
+}
 // MARK: Hide Keyboard
 #if canImport(UIKit)
 extension View {
