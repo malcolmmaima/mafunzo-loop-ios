72cb1dbf0c1c16852a9c8e3435412206b289d833
joshua kilaha <jokilaha191@gmail.com>
2022-07-26 15:14:59 +0300

    Event Cell Init

---
+++ Mafunzo Loop/View/EventListViewCell.swift
@@ -0,0 +1,60 @@
+//
+//  EventListViewCell.swift
+//  Mafunzo Loop
+//
+//  Created by Mroot on 22/07/2022.
+//
+
+import SwiftUI
+
+struct EventListViewCell: View {
+    @State var date: Int
+    @State var title: String
+    @State var startingTime: Int
+    @State var ending: Int
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(getDate())
+                    .foregroundColor(.blue)
+            }
+            .padding()
+            .background(.yellow)
+            VStack {
+                Text(title)
+                    .font(.title3)
+                    .lineLimit(1)
+                Text("\(eventTime(time:startingTime)) - \(eventTime(time: ending))")
+                    .foregroundColor(.blue)
+            }
+        }
+       .padding()
+       .frame(maxWidth: .infinity)
+        .background(
+            RoundedCornersShape(corners: .allCorners, radius: 10)
+                .fill(Color.homeCategory)
+        )
+        .shadow(radius: 1)
+    }
+    // MARK: DATE CONVERSION
+    //to Day & Month (25th July)
+    func getDate() -> String {
+        let day = Date(timeIntervalSince1970: (Double(startingTime) / 1000.0))
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "d MMM"
+        return dateFormatter.string(from: day)
+    }
+    //to Hour & Min (10:00 AM/PM)
+    func eventTime(time: Int) -> String {
+        let stating = Date(timeIntervalSince1970: (Double(time) / 1000.0))
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "h:mm a"
+        return dateFormatter.string(from: stating)
+    }
+}
+
+struct EventListViewCell_Previews: PreviewProvider {
+    static var previews: some View {
+        EventListViewCell(date: 1660719600000, title: "School Trip", startingTime: 1660719600000, ending: 1660730400000)
+    }
+}
