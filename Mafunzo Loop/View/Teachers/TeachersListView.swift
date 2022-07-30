18c78c5ac61e08bd72ef4b7c8adec846df9bfc75
joshua kilaha <jokilaha191@gmail.com>
2022-07-30 15:30:17 +0300

    View Init

---
+++ Mafunzo Loop/View/Teachers/TeachersListView.swift
@@ -0,0 +1,20 @@
+//
+//  TeachersListView.swift
+//  Mafunzo Loop
+//
+//  Created by Mroot on 30/07/2022.
+//
+
+import SwiftUI
+
+struct TeachersListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TeachersListView_Previews: PreviewProvider {
+    static var previews: some View {
+        TeachersListView()
+    }
+}
