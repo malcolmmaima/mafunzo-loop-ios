e144340d4c906c4d80a2a9b4f84f53319fbca8f3
joshua kilaha <jokilaha191@gmail.com>
2022-08-10 22:41:44 +0300

    Dictionary Value

--- Mafunzo Loop/Extensions.swift
+++ Mafunzo Loop/Extensions.swift
@@ -144,3 +144,9 @@ extension View {
         modifier(BorderedCaption())
     }
 }
+
+extension Dictionary where Value: Equatable {
+    func someKey(forValue val: Value) -> Key? {
+        return first(where: { $1 == val })?.key
+    }
+}
