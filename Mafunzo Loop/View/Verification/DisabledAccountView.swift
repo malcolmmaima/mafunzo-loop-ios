6cae713973dcfcebd1e7bdf0df051884793b99b0
joshua kilaha <jokilaha191@gmail.com>
2022-08-12 12:16:17 +0300

    Lougout

--- Mafunzo Loop/View/Verification/DisabledAccountView.swift
+++ Mafunzo Loop/View/Verification/DisabledAccountView.swift
@@ -9,28 +9,43 @@ import SwiftUI
 import Firebase
 
 struct DisabledAccountView: View {
+    @State var logout: Bool = false
     var body: some View {
-        VStack(alignment: .center) {
-            Spacer()
-            Image("disabled_account")
-                .resizable()
-                .frame(width: 150, height: 150)
-            Text("Your Account has been disabled")
-                .bold()
-            Text("Please contact support mafunzoloop@gmail.com.")
-                .font(.body)
-            
-            Spacer()
-            Button {
-                print("")
-            } label: {
-                Text("Logout")
-                    
+        NavigationView {
+            VStack(alignment: .center) {
+                Spacer()
+                Image("disabled_account")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                Text("Your Account has been disabled")
+                    .bold()
+                Text("Please contact support mafunzoloop@gmail.com.")
+                    .font(.body)
+                Spacer()
+                Button {
+                   Logout()
+                } label: {
+                    Text("Logout")
+                }
+                .frame(alignment: .bottom)
+                .padding()
+                .padding(.bottom, 50)
             }
-            .frame(alignment: .bottom)
-            .padding()
-            .padding(.bottom, 50)
+            .fullScreenCover(isPresented: $logout) {
+                    LoginView()
+            }
         }
+    }
+    func Logout() {
+            do {
+                UserDefaults.standard.reset()
+                @AppStorage("log_status") var log_status = true
+                try Auth.auth().signOut()
+                logout.toggle()
+            } catch {
+                print("already logged out")
+                
+            }
     }
 }
 
