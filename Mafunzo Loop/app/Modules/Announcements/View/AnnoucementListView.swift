5e985358f3c986ed446c7dc8546e75351b89e003
joshua kilaha <jokilaha191@gmail.com>
2022-09-13 20:17:39 +0300

    Announcement struct Update

---
+++ Mafunzo Loop/app/Modules/Announcements/View/AnnoucementListView.swift
@@ -0,0 +1,79 @@
+//
+//  AnnoucementView.swift
+//  Mafunzo Loop
+//
+//  Created by Mroot on 18/07/2022.
+//
+import SwiftUI
+
+struct AnnoucementListView: View {
+    @StateObject var annoucementViewModel = AnnoucementViewModel()
+    var body: some View {
+        GeometryReader { geo in
+            ZStack {
+                VStack {
+                   topView
+                        .frame(height: geo.size.height * 0.09)
+                    bottomView
+                }
+                .overlay {
+                    ProgressView()
+                        .opacity(annoucementViewModel.isLoading ? 1 : 0)
+                        .font(.system(size: 2))
+                        .padding()
+                        .progressViewStyle(CircularProgressViewStyle(tint: Color.yellow))
+                }
+                noAnnouncement
+            }
+        }
+        .navigationBarTitle(Text("Announcements"), displayMode: .inline)
+    }
+}
+
+struct AnnoucementView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnnoucementListView()
+    }
+}
+
+private extension AnnoucementListView {
+    // MARK: -Top View
+    var topView: some View {
+        VStack {
+            Text("")
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.yellow)
+    }
+    
+    // MARK: -Bottom View
+    var bottomView: some View {
+        VStack {
+            List {
+                ForEach(annoucementViewModel.announcements , id: \.announcementTitle) { annoucements in
+                    NavigationLink(destination: AnnouncementView(announcement: annoucements)) {
+                        AnnouncementListViewCell(image: annoucements.announcementImage, Title: annoucements.announcementTitle, date: annoucements.announcementTime, announcementBody: annoucements.announcementBody)
+                    }
+                    .listRowSeparator(.hidden)
+                }
+                .listRowBackground(Color.ViewBackground)
+            }
+            .listStyle(.plain)
+            .padding(.top, 25)
+            .refreshable {
+                annoucementViewModel.fetchAnnoucements()
+            }
+        }
+        .padding()
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .background(
+            RoundedCornersShape(corners: .allCorners, radius: 40)
+                .fill(Color.ViewBackground)
+        )
+        .offset(y: -55)
+    }
+    
+    var noAnnouncement: some View {
+        Text("No Announcement")
+            .opacity(annoucementViewModel.noAnnouncement ? 1 : 0)
+    }
+}
---
+++ Mafunzo Loop/app/Modules/Announcements/View/AnnouncementView.swift
@@ -0,0 +1,92 @@
+//
+//  AnnouncementItemView.swift
+//  Mafunzo Loop
+//
+//  Created by Mroot on 19/07/2022.
+//
+import SwiftUI
+
+struct AnnouncementView: View {
+    var announcement: Annoucements
+    var body: some View {
+            GeometryReader { geo in
+                VStack(spacing: 5) {
+                   topView
+                        .frame(height: geo.size.height * 0.1)
+                    bottomView
+                }
+        }
+    }
+    func dateFormattered() -> String {
+        let timeStamp = Date(timeIntervalSince1970: TimeInterval(announcement.announcementTime))
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "d MMM, h:mm a"
+        return dateFormatter.string(from: timeStamp)
+    }
+}
+
+//struct AnnouncementItemView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        AnnouncementItemView()
+//    }
+//}
+
+private extension AnnouncementView {
+    // MARK: -Top View
+    var topView: some View {
+        VStack {
+            Text("")
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.yellow)
+    }
+    
+    // MARK: -Bottom View
+    var bottomView: some View {
+        ScrollView {
+            VStack(alignment: .leading) {
+                title
+                time
+                image
+                bodyAnnouncement
+            }
+            .padding()
+            .frame(minWidth: 0, maxWidth: .infinity)
+       }
+        .background(
+            RoundedCornersShape(corners: .allCorners, radius: 40)
+                .fill(Color.ViewBackground)
+        )
+        .offset(y: -55)
+    }
+    
+    // MARK: title
+    var title: some View {
+        Text(announcement.announcementTitle)
+            .font(.title2)
+            .padding()
+    }
+    
+    // MARK: time
+    @ViewBuilder
+    var time: some View {
+        Text(dateFormattered())
+            .foregroundColor(.blue)
+        Divider()
+    }
+    
+    // MARK: image
+    var image: some View {
+        AsyncImage(url: URL(string: announcement.announcementImage)) { image in
+            image.resizable()
+        } placeholder: {
+            ProgressView()
+        } .frame(maxWidth: .infinity, maxHeight: 350)
+    }
+    
+    // MARK: body
+    var bodyAnnouncement: some View {
+        Text(announcement.announcementBody)
+            .font(.body)
+            .fixedSize(horizontal: false, vertical: true)
+    }
+}
