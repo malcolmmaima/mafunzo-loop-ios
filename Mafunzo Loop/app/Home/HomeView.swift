0f9b46ce2641b8f4fc3daa00f4e8f1af39771a6b
joshua kilaha <jokilaha191@gmail.com>
2022-12-10 11:37:32 +0300

    System Offline

--- Mafunzo Loop/app/Home/HomeView.swift
+++ Mafunzo Loop/app/Home/HomeView.swift
@@ -13,6 +13,7 @@ struct HomeView: View {
     @StateObject var userViewModel = UserViewModel()
     @StateObject var schoolViewModel = SchoolViewModel()
     @ObservedObject var networkManager = NetworkViewModel()
+    @StateObject var systemState = SystemSettingsViewModel()
     @ObservedObject var user: User
     @State var settings: Bool = false
     @State var logout: Bool = false
@@ -21,171 +22,175 @@ struct HomeView: View {
     @State private var showWorkSpace = false
     @State private var blurRadius: Double = 1.0
     var body: some View {
-        ZStack {
-            NavigationView {
-                GeometryReader { geo in
-                    VStack {
-                       // MARK: TopView
-                        HStack {
-                            Text("Hi, \(userViewModel.name)")
-                                .foregroundColor(.white)
-                                .font(.title2)
-                                .bold()
-                            Spacer()
-                            Menu {
-                                Button("Settings") {
-                                    self.settings = true
-                                }
-                                Button("Logout") {
-                                    showLogoutAlert = true
-                                }
-                            } label: {
-                                Image(systemName: "person.circle.fill")
-                                    .resizable()
-                                    .frame(width: 45, height: 45)
-                                    .foregroundColor(Color.buttonHomeColor)
-                            }
-                        }.padding()
-                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-                            .padding()
-                            .background(Color.yellow)
-                            .frame(height: geo.size.height * 0.25)
-                            .blur(radius: showWorkSpace ? 10 : 0)
-                            .disabled(showWorkSpace ? true : false)
-                        // MARK: School Modules
+        if systemState.systemStatus != false {
+            SystemOfflineView()
+        } else {
+            ZStack {
+                NavigationView {
+                    GeometryReader { geo in
                         VStack {
-                            ScrollView {
-                                ZStack {
-                                    HomeCell()
-                                        .blur(radius: showWorkSpace ? 5 : 0)
-                                        .disabled(showWorkSpace ? true : false)
-                                        .opacity(userViewModel.schoolStatus ? 1 : 0)
-                                    VStack {
-                                        // MARK: -WORKSPACE ERROR!!! VIEW
-                                        WorkSpaceErrorView()
-                                        Text("Contact your school to activate your workspace")
-                                            .foregroundColor(.red)
-                                        Spacer()
+                            // MARK: TopView
+                            HStack {
+                                Text("Hi, \(userViewModel.name)")
+                                    .foregroundColor(.white)
+                                    .font(.title2)
+                                    .bold()
+                                Spacer()
+                                Menu {
+                                    Button("Settings") {
+                                        self.settings = true
                                     }
-                                    .opacity(userViewModel.schoolStatus ? 0 : 1)
-                                    // MARK: -WORKSPACE VIEW SELECT
-                                    //WorkSpaceView()
-                                    VStack {
-                                        VStack {
-                                            Text("Switch WorkSpace")
-                                                .bold()
-                                                .font(.title)
-                                                .padding()
-                                            List {
-                                                ForEach(schoolViewModel.selectedSchool, id: \.id) { school in
-                                                    Text(school.schoolName)
-                                                        .frame(alignment: .leading)
-                                                        .padding(.bottom, 1)
-                                                        .listRowSeparator(.hidden)
-                                                        .onTapGesture {
-                                                            let schoolID = school.id
-                                                            //userViewModel.schoolStored = schoolID
-                                                            UserDefaults.standard.set(schoolID, forKey: "schoolID")
-                                                            userViewModel.getSchoolIDFromDetails(schoolIDStore: schoolID)
-                                                            userViewModel.getUserDetails(schoolIDStore: schoolID)
-                                                            print("School Id Picked: \(userViewModel.schoolStored)")
-                                                            print("School Name Selected: \(school.schoolName)")
-                                                        }
-                                                }.listRowBackground(Color.clear)
-                                            }
-                                            .listStyle(.plain)
-                                            Spacer()
-                                            NavigationLink {
-                                                // MARK: SCHOOL BUTTON
-                                                SchoolListView()
-                                            } label: {
-                                                Text("Add School")
-                                                    .font(.body)
-                                                    .foregroundColor(.white)
-                                                    .frame(width: 250, height: 35)
-                                            }
-                                            .background(Color.blue)
-                                            .padding(.bottom, 10)
-                                        }
+                                    Button("Logout") {
+                                        showLogoutAlert = true
                                     }
-                                    .frame(width: 300, height: 280, alignment: .center)
-                                    .background(
-                                        RoundedCornersShape(corners: .allCorners, radius: 15)
-                                            .fill(Color.homeCategory)
-                                    )
-                                    .shadow(radius: 2.0)
-                                    .onAppear {
-                                        schoolViewModel.getUserSchools()
-                                    }
-                                    .opacity(showWorkSpace ? 1 : 0)
-                                    // MARK: -
+                                } label: {
+                                    Image(systemName: "person.circle.fill")
+                                        .resizable()
+                                        .frame(width: 45, height: 45)
+                                        .foregroundColor(Color.buttonHomeColor)
                                 }
+                            }.padding()
+                                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+                                .padding()
+                                .background(Color.yellow)
+                                .frame(height: geo.size.height * 0.25)
+                                .blur(radius: showWorkSpace ? 10 : 0)
+                                .disabled(showWorkSpace ? true : false)
+                            // MARK: School Modules
+                            VStack {
+                                ScrollView {
+                                    ZStack {
+                                        HomeCell()
+                                            .blur(radius: showWorkSpace ? 5 : 0)
+                                            .disabled(showWorkSpace ? true : false)
+                                            .opacity(userViewModel.schoolStatus ? 1 : 0)
+                                        VStack {
+                                            // MARK: -WORKSPACE ERROR!!! VIEW
+                                            WorkSpaceErrorView()
+                                            Text("Contact your school to activate your workspace")
+                                                .foregroundColor(.red)
+                                            Spacer()
+                                        }
+                                        .opacity(userViewModel.schoolStatus ? 0 : 1)
+                                        // MARK: -WORKSPACE VIEW SELECT
+                                        //WorkSpaceView()
+                                        VStack {
+                                            VStack {
+                                                Text("Switch WorkSpace")
+                                                    .bold()
+                                                    .font(.title)
+                                                    .padding()
+                                                List {
+                                                    ForEach(schoolViewModel.selectedSchool, id: \.id) { school in
+                                                        Text(school.schoolName)
+                                                            .frame(alignment: .leading)
+                                                            .padding(.bottom, 1)
+                                                            .listRowSeparator(.hidden)
+                                                            .onTapGesture {
+                                                                let schoolID = school.id
+                                                                //userViewModel.schoolStored = schoolID
+                                                                UserDefaults.standard.set(schoolID, forKey: "schoolID")
+                                                                userViewModel.getSchoolIDFromDetails(schoolIDStore: schoolID)
+                                                                userViewModel.getUserDetails(schoolIDStore: schoolID)
+                                                                print("School Id Picked: \(userViewModel.schoolStored)")
+                                                                print("School Name Selected: \(school.schoolName)")
+                                                            }
+                                                    }.listRowBackground(Color.clear)
+                                                }
+                                                .listStyle(.plain)
+                                                Spacer()
+                                                NavigationLink {
+                                                    // MARK: SCHOOL BUTTON
+                                                    SchoolListView()
+                                                } label: {
+                                                    Text("Add School")
+                                                        .font(.body)
+                                                        .foregroundColor(.white)
+                                                        .frame(width: 250, height: 35)
+                                                }
+                                                .background(Color.blue)
+                                                .padding(.bottom, 10)
+                                            }
+                                        }
+                                        .frame(width: 300, height: 280, alignment: .center)
+                                        .background(
+                                            RoundedCornersShape(corners: .allCorners, radius: 15)
+                                                .fill(Color.homeCategory)
+                                        )
+                                        .shadow(radius: 2.0)
+                                        .onAppear {
+                                            schoolViewModel.getUserSchools()
+                                        }
+                                        .opacity(showWorkSpace ? 1 : 0)
+                                        // MARK: -
+                                    }
+                                }
+                                .frame(height: geo.size.height * 0.7)
+                                .offset(y: -50)
+                                .onTapGesture {
+                                    showWorkSpace = false //diable Workspace View
+                                }
+                                
+                                Button {
+                                    // MARK: SCHOOL BUTTON
+                                    showWorkSpace = true
+                                } label: {
+                                    Text(userViewModel.fetchedSchool)
+                                        .font(.body)
+                                        .foregroundColor(Color.buttonHomeColor)
+                                        .frame(width: 350, height: 35)
+                                }
+                                .background(
+                                    RoundedCornersShape(corners: .allCorners, radius: 20)
+                                        .fill(Color.homeCategory)
+                                )
+                                .shadow(radius: 1)
+                                // MARK: Network Listener
+                                //                             if networkManager.isNotConnected {
+                                //                                 NetworkViewCell(netStatus: networkManager.conncetionDescription, image: networkManager.imageName)
+                                //                             }
                             }
-                            .frame(height: geo.size.height * 0.7)
-                           .offset(y: -50)
-                           .onTapGesture {
-                               showWorkSpace = false //diable Workspace View
-                           }
-                            
-                            Button {
-                                // MARK: SCHOOL BUTTON
-                                showWorkSpace = true
-                            } label: {
-                                Text(userViewModel.fetchedSchool)
-                                    .font(.body)
-                                    .foregroundColor(Color.buttonHomeColor)
-                                    .frame(width: 350, height: 35)
-                            }
+                            .padding()
+                            // MARK: Curving view
                             .background(
-                                RoundedCornersShape(corners: .allCorners, radius: 20)
-                                    .fill(Color.homeCategory)
+                                RoundedCornersShape(corners: .allCorners, radius: 45)
+                                    .fill(Color.ViewBackground)
                             )
-                            .shadow(radius: 1)
-                            // MARK: Network Listener
-//                             if networkManager.isNotConnected {
-//                                 NetworkViewCell(netStatus: networkManager.conncetionDescription, image: networkManager.imageName)
-//                             }
+                            .offset(y: -50)
                         }
-                        .padding()
-                     // MARK: Curving view
-                         .background(
-                             RoundedCornersShape(corners: .allCorners, radius: 45)
-                                .fill(Color.ViewBackground)
-                         )
-                         .offset(y: -50)
+                        .background(Color.ViewBackground)
+                        .navigationBarHidden(true)
                     }
-                    .background(Color.ViewBackground)
                     .navigationBarHidden(true)
-                }
-                .navigationBarHidden(true)
-                .background(
-                    NavigationLink(destination: Settings(), isActive: $settings) {
-                        EmptyView()
-                })
-                .fullScreenCover(isPresented: $logout) {
+                    .background(
+                        NavigationLink(destination: Settings(), isActive: $settings) {
+                            EmptyView()
+                        })
+                    .fullScreenCover(isPresented: $logout) {
                         LoginView()
+                    }
                 }
+                .opacity(userViewModel.userStatus ? 1 : 0)
+                // MARK: Logout Alert
+                .alert(isPresented: $showLogoutAlert) {
+                    Alert(
+                        title: Text("Logout!!"),
+                        message: Text("Are you sure you want to logout?"),
+                        primaryButton: .default(
+                            Text("Cancel")
+                        ),
+                        secondaryButton: .destructive(Text("Logout"), action: {
+                            Logout()
+                            logout.toggle()
+                        }))
+                }
+                .onAppear {
+                    userViewModel.getUserDetails(schoolIDStore: userViewModel.schoolStored)
+                    userViewModel.getSchoolIDFromDetails(schoolIDStore: userViewModel.schoolStored)
+                }
+                DisabledAccountView()
+                    .opacity(userViewModel.userStatus ? 0 : 1)
             }
-            .opacity(userViewModel.userStatus ? 1 : 0)
-            // MARK: Logout Alert
-            .alert(isPresented: $showLogoutAlert) {
-                Alert(
-                    title: Text("Logout!!"),
-                    message: Text("Are you sure you want to logout?"),
-                    primaryButton: .default(
-                        Text("Cancel")
-                    ),
-                    secondaryButton: .destructive(Text("Logout"), action: {
-                    Logout()
-                    logout.toggle()
-                }))
-            }
-            .onAppear {
-                userViewModel.getUserDetails(schoolIDStore: userViewModel.schoolStored)
-                userViewModel.getSchoolIDFromDetails(schoolIDStore: userViewModel.schoolStored)
-            }
-            DisabledAccountView()
-                .opacity(userViewModel.userStatus ? 0 : 1)
         }
     }
     func Logout() {
